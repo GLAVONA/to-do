221d3ef6b707a62b1672de2c7ff0b29f62d64c64
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2022-12-01 23:55:27 +0200

    Add project and delete project buttons coded

--- src/index.js
+++ src/index.js
@@ -1,13 +1,65 @@
 import datefns, { endOfMonth, endOfToday } from 'date-fns';
 import './style.css';
-import Project, { addTaskToProject, removeTaskFromProject } from './project.js';
+import Project, { addTaskToProject, createNewProject, projectArray, removeTaskFromProject } from './project.js';
 import Task, { checkTask } from './task.js';
 
-const projectCollapsible = document.querySelector(".collapse");
-projectCollapsible.addEventListener("click",()=>{
-    const projectList = document.querySelector(".projects-list");
-    projectList.style.display = projectList.style.display == "flex"? "none" : "flex";
-    projectList.style.display == "flex"? projectCollapsible.innerHTML = "Projects<br>⌄" : projectCollapsible.innerHTML = "Projects<br>-"
-})
+const projectCollapsible = document.querySelector(".projects-collapse");
+projectCollapsible.addEventListener("click",toggleProjects)
 
-//<div class="project" onclick="this.contentEditable='true';" onblur="this.contentEditable='false';">Project 1</div>
+
+const projectList = document.querySelector(".projects-list");
+const projectWrapper = document.querySelector(".project-wrapper");
+const addProjectButton = document.querySelector(".add-project")
+
+function toggleProjects(){
+    projectList.style.display = projectList.style.display == "flex"? "none" : "flex";
+    projectList.style.display == "flex"? projectCollapsible.innerHTML = "Projects<br>⌄" : projectCollapsible.innerHTML = "Projects<br>-";
+    addProjectButton.style.display = addProjectButton.style.display == "flex"? "none" : "flex";
+}
+
+
+
+function renderProjects(){
+    function constructProjectDiv(project){
+        const projectWrapper = document.createElement("div");
+        projectWrapper.classList.add("project-wrapper");
+        projectWrapper.id = "0";
+
+        const projectDiv = document.createElement("div");
+        projectDiv.classList.add("project");
+        projectDiv.setAttribute("onclick","this.contentEditable='true'");
+        projectDiv.setAttribute("onblur", "this.contentEditable='false'");
+        projectDiv.textContent = project.name;
+        
+        const deleteButton = document.createElement("button");
+        deleteButton.setAttribute("type","button");
+        deleteButton.textContent = "X";
+        deleteButton.addEventListener("click",()=>{
+            deleteButton.parentElement.remove();
+            projectArray.splice(projectArray.indexOf(deleteButton.parentElement.id, 1));
+        })
+
+        projectWrapper.appendChild(projectDiv);
+        projectWrapper.appendChild(deleteButton);
+        return projectWrapper;
+    }
+    let id = 0;
+    projectArray.forEach((proj)=>{
+        const newProj = constructProjectDiv(proj);
+        projectList.appendChild(newProj);
+        newProj.id=id;
+        id++;
+    })
+    console.log(projectArray);
+}
+
+function clearProjectList(){
+    projectList.innerHTML="";
+}
+
+
+addProjectButton.addEventListener("click",()=>{
+    createNewProject();
+    clearProjectList();
+    renderProjects()
+})
--- src/project.js
+++ src/project.js
@@ -1,5 +1,7 @@
 import Note from './notes.js';
 
+export const projectArray = [];
+
 export default function Project(name){
     const taskArray = [];
     const note = Note("");
@@ -18,3 +20,12 @@ export const moveTaskToAnotherProject = (task, oldProject, newProject)=>{
     addTaskToProject(task,newProject);
     removeTaskFromProject(task,oldProject);
 }
+
+
+export function createNewProject(){
+    const nextProjectNumber = document.querySelector(".projects-list").childElementCount+1;
+    const newProject = Project(`Project ${projectArray.length+1}`);
+    projectArray.push(newProject);
+    return newProject;
+}
+    //<div class="project" onclick="this.contentEditable='true';" onblur="this.contentEditable='false';">Project 1</div>
