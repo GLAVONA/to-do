1ed0a5e8abcf8134b8698532329eecc17e8ff0d5
GLAVONA <97589269+GLAVONA@users.noreply.github.com>
2023-01-16 08:52:48 +0200

    added firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAk5DRgESsSCJKEU-J2ADwPzYBK5TQIO6Q",
+  authDomain: "to-do-top.firebaseapp.com",
+  projectId: "to-do-top",
+  storageBucket: "to-do-top.appspot.com",
+  messagingSenderId: "835208271830",
+  appId: "1:835208271830:web:e8c09086217b7e283cb677"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
